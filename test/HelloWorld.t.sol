33732ca9420c5907679f5e1ac0439bb1d6dd802e
mymiracle0118 <noahsflood908@gmail.com>
2024-01-26 10:48:16 +0900

    testTargetClose finish

--- test/HelloWorld.t.sol
+++ test/HelloWorld.t.sol
@@ -28,45 +28,42 @@ contract TestHelloWorld is Test {
         accessManager = new MockAccessManager(admin);
         helloWorld = new MockHelloWorld(address(accessManager));
 
-        vm.prank(admin);
+        vm.startPrank(admin);
         accessManager.setGrantDelay(Roles.ADMIN, GRANT_DELAY);
 
-        vm.prank(admin);
         accessManager.grantRole(Roles.ADMIN, admin, EXECUTION_DELAY);
 
         accessManager.setRoleAdmin(Roles.ADMIN, Roles.ADMIN);
+
         accessManager.setRoleAdmin(Roles.GOVERNOR, Roles.ADMIN);
         accessManager.setRoleAdmin(Roles.USER, Roles.ADMIN);
         accessManager.setRoleAdmin(Roles.GUARDIAN, Roles.ADMIN);
 
-        accessManager.labelRole(Roles.ADMIN, "ADMIN");
+        vm.stopPrank();
+
+        // accessManager.labelRole(Roles.ADMIN, "ADMIN");
     }
 
     function testTargetClose() public {
-        vm.prank(admin);
+        vm.startPrank(admin);
         accessManager.setTargetClosed(address(helloWorld), true);
-
-        vm.expectRevert();
-        vm.prank(admin);
-        helloWorld.hello();
-
-        vm.prank(admin);
-        accessManager.setTargetClosed(address(helloWorld), false);
-
         assertEq(accessManager.isTargetClosed(address(helloWorld)), true);
 
         vm.expectRevert();
         helloWorld.hello();
 
-        vm.expectEmit(true, true, false, true, address(helloWorld));
-        emit HelloWorld();
-
+        accessManager.setTargetClosed(address(helloWorld), false);
         assertEq(accessManager.isTargetClosed(address(helloWorld)), false);
+
+        // vm.expectEmit(true, true, false, true, address(helloWorld));
+        // emit HelloWorld();
+        helloWorld.hello();
+        vm.stopPrank();
     }
 
     function testRoleRestriction() public {
         vm.prank(admin);
-        bool isGoverner = false;
+        bool isGoverner;
         uint256 roleDelay;
         accessManager.grantRole(Roles.GOVERNOR, alice, EXECUTION_DELAY);
         (isGoverner, roleDelay) = accessManager.hasRole(Roles.GOVERNOR, alice);
@@ -75,17 +72,29 @@ contract TestHelloWorld is Test {
         assertEq(roleDelay, EXECUTION_DELAY);
 
         bytes4[] memory selectors;
+        selectors = new bytes4[](1);
         selectors[0] = bytes4(keccak256("hello()"));
+
+        vm.prank(admin);
         accessManager.setTargetFunctionRole(
             address(helloWorld),
             selectors,
             Roles.GOVERNOR
         );
+
+        console.log("================test===============");
+        // vm.expectEmit(true, true, false, true, address(helloWorld));
+        // emit HelloWorld();
+
+        vm.warp(block.timestamp + EXECUTION_DELAY);
+
+        accessManager.schedule(
+            address(helloWorld),
+            selectors,
+            block.timestamp + EXECUTION_DELAY
+        );
+
         vm.prank(alice);
-
-        vm.expectEmit(true, true, false, true, address(helloWorld));
-        emit HelloWorld();
-
         helloWorld.hello();
 
         vm.warp(block.timestamp + EXECUTION_DELAY);
